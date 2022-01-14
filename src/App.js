be981cf60768a63b8eec235e6e37267ec68894a2
atemp678 <ajt2158@columbia.edu>
2022-01-14 16:18:06 +0000

    somethings didn't really work out

--- src/App.js
+++ src/App.js
@@ -3,30 +3,31 @@ import React, { useEffect, useState } from "react";
 import fetchCocktail from "./data";
 
 function App() {
-   const [cocktail, setCocktail] = useState("");
+  const [cocktail, setCocktail] = useState("");
 
-   useEffect(async () => {
-      setCocktail(await fetchCocktail());
-   }, []);
+  useEffect(async () => {
+    setCocktail(await fetchCocktail());
+  }, []);
 
-   //I know it's complaining but it works 🤷
+  //I know it's complaining but it works 🤷
 
-   // console.log("detail info", cocktail.drinks);
+  // console.log("detail info", cocktail.drinks);
 
-   return cocktail ? (
-      <div className="App">
-         <header className="App-header">
-            <h1>Is this your drink?</h1>
+  return cocktail ? (
+    <div className="App">
+      <header className="App-header">
+        <h1>Is this your drink?</h1>
 
-            <h2>{cocktail[1][1]}</h2>
-            <img src={cocktail[16][1]} className="App-logo" alt="logo" />
-            <h3>Ingredients</h3>
-            <p></p>
-         </header>
-      </div>
-   ) : (
-      <></>
-   );
+        <h2>{cocktail[1][1]}</h2>
+        <img src={cocktail[16][1]} className="App-logo" alt="logo" />
+        <h3>Ingredients</h3>
+
+        <p></p>
+      </header>
+    </div>
+  ) : (
+    <></>
+  );
 }
 
 export default App;
